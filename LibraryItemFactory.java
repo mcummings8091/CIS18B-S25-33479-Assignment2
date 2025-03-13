faf31e53d95e69b1d92f7da010d4b5d2f36d6864
Matthew Cummings <mcummings8091@gmail.com>
2025-03-13 03:54:18 -0700

    Refactoring

--- LibraryItemFactory.java
+++ LibraryItemFactory.java
@@ -1,19 +1,26 @@
 public abstract class LibraryItemFactory {
+    private ItemBuilder builder;
+    
+    public LibraryItemFactory(ItemBuilder builder) {
+        this.builder = builder;
+    }
     public Item createItem(String type, String title, int publicationYear, String extraData) {
         if (type.equals("book")) {
-            Book book = new Book();
-            book.setTitle(title);
-            book.setAuthor(extraData);
-            book.setPublicationYear(publicationYear);
-            return book;
+            BookBuilder bookBuilder = new BookBuilder();
+            bookBuilder.buildTitle(title);
+            bookBuilder.buildAuthor(extraData);
+            bookBuilder.buildPublicationYear(publicationYear);
+            return bookBuilder.getResult();
         } else {
-            Magazine magazine = new Magazine();
-            magazine.setTitle(title);
-            magazine.setPublicationYear(publicationYear);
-            magazine.setIssueNumber(publicationYear);
-            return magazine;
+            MagazineBuilder magazineBuilder = new MagazineBuilder();
+            magazineBuilder.buildTitle(title);
+            magazineBuilder.buildPublicationYear(publicationYear);
+            magazineBuilder.buildIssueNumber(Integer.parseInt(extraData));
+            return magazineBuilder.getResult();
         }
     }
     
     public abstract Item buildItem();
 }
+
+
